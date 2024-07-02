66bed4485bc3d0fe22a3793eb037350eb709b84f
Taylor Jadin <jadin.taylor@gmail.com>
2024-07-02 16:07:53 -0500

    missing Y

--- install.sh
+++ install.sh
@@ -2,9 +2,12 @@
 
 yum update -y
 yum -y install curl --allowerasing
+
+echo -e "
 sh <(curl https://cyberpanel.net/install.sh || wget -O - https://cyberpanel.net/install.sh) << EOF
 1
 1
+Y
 N
 <ENTER>
 n
